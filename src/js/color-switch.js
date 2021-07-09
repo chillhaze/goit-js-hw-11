eb91cf3b5f4243b8502be4098a75adee6c2a5d02
chillhaze <65725016+chillhaze@users.noreply.github.com>
2021-07-09 16:35:32 +0300

    hw-11-task-1 added

---
+++ src/js/color-switch.js
@@ -0,0 +1,28 @@
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button.btn-start'),
+  stop: document.querySelector('button.btn-stop'),
+};
+let changeColorTimer = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const onStartBtnClick = evt => {
+  refs.start.setAttribute('disabled', true);
+  changeColorTimer = setInterval(() => {
+    let randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    refs.body.style.backgroundColor = randomColor;
+  }, 1000);
+};
+
+const onStopBtnClick = () => {
+  refs.start.removeAttribute('disabled');
+  clearInterval(changeColorTimer);
+};
+
+refs.start.addEventListener('click', onStartBtnClick);
+refs.stop.addEventListener('click', onStopBtnClick);
